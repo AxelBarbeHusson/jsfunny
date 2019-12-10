fa3eb944355e35089af6179ce233a83d089f4410
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-12-10 12:53:21 +0100

    LeapYear function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,2 @@
-let valeur = prompt('Veuillez saisir un nombre');
-let msg = '';
-if (!isNaN(valeur)) {
-    if (valeur > 0) {
-        msg = 'Positif';
-    } else if (valeur < 0) {
-        msg = 'Negatif';
-    } else {
-        msg = 'Nul';
-    }
-} else msg = 'Un nombre c\'est un chiffre connard !';
-console.log(msg);
-document.write(msg);
+let bordel = document.getElementById('truc').innerText ='coucou';
+let bordel = document.getElementById('truc').style.color ='#FF0000';
