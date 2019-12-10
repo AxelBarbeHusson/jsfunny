85f1de7bf8ce0328382bee16459469f5230c73a2
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-12-10 14:11:06 +0100

    bièere

---
+++ assets/js/function/biere.js
@@ -0,0 +1,8 @@
+function biere(hauteur,rayon) {
+    if ((!isNaN(rayon) && !isNaN(hauteur)) && (rayon >= 0 && hauteur >= 0)){
+        return hauteur * Math.PI * Math.pow(rayon,2);
+
+    }else {
+        return false;
+    }
+}
