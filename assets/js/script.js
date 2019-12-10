a40ae692c276e23f8db63f45c8079400c6f9127a
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-12-10 10:41:31 +0100

    alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-document.write('Michel');
+
+let message ="<ul>";
+
+for(let i = -1722; i <=2756 ; i++){
+    if(i % 9 === 0){
+        message += '<li>';
+        message += i;
+        message += '</li>';
+
+    }
+}
+
+message += '</ul>';
+alert('Boujou !');
+document.write(message);
